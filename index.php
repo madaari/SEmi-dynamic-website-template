8ddb9efb78f3e3cae73c55e187a1e014906fcc36
Udit agarwal <uditagarwal1997@gmail.com>
2017-09-09 15:12:06 +0530

    Added Nav bar

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="shortcut icon" type="image/x-icon" href="img/favicon.png" />
+	<link rel="shortcut icon" type="image/x-icon" href="img/logo.png" />
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/device_specific.css">
 	<link rel="stylesheet" href="css/common.css">
@@ -13,20 +13,90 @@
 	</title>
 	<meta name="description" content="Official website of Center of Electronic Design and Technology">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<style type="text/css">
+
+		h4{
+			font-size: 1.3em;
+		}
+		h5{
+			font-size: 1.2em;
+		}
+        body {
+            padding-top: 50px;
+        }
+        .navbar-template {
+            padding: 40px 15px;
+        }
+        .navbar-default {
+			 background-color: #e1bee7;
+			 border-color: #ec5252;
+		}
+		@media screen and (min-width:320px) and (max-width:700px) and (orientation:portrait) {
+		
+	  		body{font-size: 10px;}
+	  		.border{
+	  			border-right-width: 0px;
+	  			border-bottom-style: dotted;
+				border-block-end-width: 1px;
+				margin-block-end: 1px;
+	  		}
+	  		.btn_no{
+			margin-top:5% ;
+			}	
+			.centerify{
+			text-align: center;
+			}
+		    .navbar-nav .dropdown-menu .caret {
+				transform: rotate(-90deg);
+		    }
+			h1{
+				font-size: 1.7em;
+			}
+			h2{
+				font-size: 1.2em;
+			}
+		}
+
+	@media screen and (min-width:320px) and (max-width:700px) and (orientation:landscape) {
+		
+	  		body{font-size: 12px;}
+	  		.border{
+	  			border-right-width: 0px;
+	  			border-bottom-style: dotted;
+				border-block-end-width: 1px;
+				margin-block-end: 1px;
+	  		}
+	  		.btn_no{
+			margin-top:5% ;
+			}
+			.centerify{
+			text-align: center;
+			}
+		    .navbar-nav .dropdown-menu .caret {
+				transform: rotate(-90deg);
+		    }
+			h1{
+				font-size: 2em;
+			}
+			h2{
+				font-size: 1.5em;
+			}
+	}
+	</style>
 </head>
 <body>
 <!-- Division 1-->
-	<div class="container-fluid" style="background-color:#eceff1; margin-left: 0.5em;margin-right: 0.5em;" >
+	<div class="container-fluid" style="background-color:#eceff1; margin-left: 0.5em;margin-right: 0.5em;height:11em" >
 		<div class="row">
 			<!-- Division 1.1 LOGO-->
 			<div id="div1-1" class="col-xs-1" style="padding: 0px;margin-right: 1em;">
-					<img class="img-responsive" src="img/logo.png" alt="logo" height=100 width=75 style="position: relative;left:7%;margin-top:1em;">
-				<div id="div1-1_link" style="text-align: center;">
+					<img class="img-responsive" src="img/logo.png" alt="logo" height=100 width=120 style="position: relative;left:7%;margin-top:1em;">
+				<div id="div1-1_link" style="text-align: center;margin-top:2em;margin-left: 0.5em;">
 					<div>
 						<h5><a class="nav-link" href="http://sevasetu.org/contribute-now/" target="_blank">Contact us</a></h5>
 					</div>
 					<div>
-						<h5><a class="nav-link" href="http://sevasetu.org/" target="_blank">About Us</a></h5>
+						<h5><a class="nav-link" href="http://sevasetu.org/" target="_blank">About us</a></h5>
 					</div>
 				</div>
 			</div>
@@ -44,6 +114,110 @@
 		</div>
 		<hr>
 </div>
+        <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">CEDT</a>
+                </div>
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#" target="_blank">Wiki</a></li>
+                        <li>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Projects<b class="caret"></b></a>
+
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Action [Menu 1.1]</a></li>
+                                <li><a href="#">Another action [Menu 1.1]</a></li>
+                                <li><a href="#">Something else here [Menu 1.1]</a></li>
+                                <li class="divider"></li>
+                                <li><a href="#">Separated link [Menu 1.1]</a></li>
+                                <li class="divider"></li>
+                                <li><a href="#">One more separated link [Menu 1.1]</a></li>
+                                <li>
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown [Menu 1.1] <b class="caret"></b></a>
+
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#">Action [Menu 1.2]</a></li>
+                                        <li>
+                                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown [Menu 1.2] <b class="caret"></b></a>
+
+                                            <ul class="dropdown-menu">
+                                                <li>
+                                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown [Menu 1.3] <b class="caret"></b></a>
+
+                                                    <ul class="dropdown-menu">
+                                                        <li><a href="#">Action [Menu 1.4]</a></li>
+                                                        <li><a href="#">Another action [Menu 1.4]</a></li>
+                                                        <li><a href="#">Something else here [Menu 1.4]</a></li>
+                                                        <li class="divider"></li>
+                                                        <li><a href="#">Separated link [Menu 1.4]</a></li>
+                                                        <li class="divider"></li>
+                                                        <li><a href="#">One more separated link [Menu 1.4]</a></li>
+                                                    </ul>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Events <b class="caret"></b></a>
+
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Action [Menu 2.1]</a></li>
+                                <li><a href="#">Another action [Menu 2.1]</a></li>
+                                <li><a href="#">Something else here [Menu 2.1]</a></li>
+                                <li class="divider"></li>
+                                <li><a href="#">Separated link [Menu 2.1]</a></li>
+                                <li class="divider"></li>
+                                <li><a href="#">One more separated link [Menu 2.1]</a></li>
+                                <li>
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown [Menu 2.1] <b class="caret"></b></a>
+
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#">Action [Menu 2.2]</a></li>
+                                        <li><a href="#">Another action [Menu 2.2]</a></li>
+                                        <li><a href="#">Something else here [Menu 2.2]</a></li>
+                                        <li class="divider"></li>
+                                        <li><a href="#">Separated link [Menu 2.2]</a></li>
+                                        <li class="divider"></li>
+                                        <li>
+                                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown [Menu 2.2] <b class="caret"></b></a>
+
+                                            <ul class="dropdown-menu">
+                                                <li>
+                                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown [Menu 2.3] <b class="caret"></b></a>
+
+                                                    <ul class="dropdown-menu">
+                                                        <li><a href="#">Action [Menu 2.4]</a></li>
+                                                        <li><a href="#">Another action [Menu 2.4]</a></li>
+                                                        <li><a href="#">Something else here [Menu 2.4]</a></li>
+                                                        <li class="divider"></li>
+                                                        <li><a href="#">Separated link [Menu 2.4]</a></li>
+                                                        <li class="divider"></li>
+                                                        <li><a href="#">One more separated link [Menu 2.4]</a></li>
+                                                    </ul>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div><!--/.nav-collapse -->
+            </div>
+        </div>
+
+   
 </body>
 <script>
 
@@ -52,8 +226,9 @@
 		function checkWidth(){
 
 			if ($window.width() <= 700) {
-				$('#div1-3').removeClass('col-xs-1');
+				$('#div1-3').removeClass('col-xs-1').addClass('col-xs-0');
 				$('#div1-3').remove();
+				$('#div1-3').detach();
 	            $('#div1-1').removeClass('col-xs-1').addClass('col-xs-2');
 	            $('#title').removeClass('decMargin');
 	            $('#areaofwork').removeClass('decMargin');
@@ -69,6 +244,19 @@
 		}
 		checkWidth();
 		$(window).resize(checkWidth);
+	   $('.navbar a.dropdown-toggle').on('click', function(e) {
+        var $el = $(this);
+        var $parent = $(this).offsetParent(".dropdown-menu");
+        $(this).parent("li").toggleClass('open');
+
+        if(!$parent.parent().hasClass('nav')) {
+            $el.next().css({"top": $el[0].offsetTop, "left": $parent.outerWidth() - 4});
+        }
+
+        $('.nav li.open').not($(this).parents("li")).removeClass("open");
+
+        return false;
+    	});
 	});
 
 </script>
