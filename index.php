9feee6a4ee7ceaf185bb18874d42e75f02f40b36
Udit agarwal <uditagarwal1997@gmail.com>
2017-09-14 01:37:23 +0530

    Improved nav bar

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 	<title>
 		CEDT 
 	</title>
-	<meta name="description" content="Official website of Center of Electronic Design and Technology">
+	<meta name="description" content="Official website of Center of Embedded Design and Technology">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<style type="text/css">
 
@@ -21,16 +21,7 @@
 		h5{
 			font-size: 1.2em;
 		}
-        body {
-            padding-top: 50px;
-        }
-        .navbar-template {
-            padding: 40px 15px;
-        }
-        .navbar-default {
-			 background-color: #e1bee7;
-			 border-color: #ec5252;
-		}
+
 		@media screen and (min-width:320px) and (max-width:700px) and (orientation:portrait) {
 		
 	  		body{font-size: 10px;}
@@ -46,15 +37,34 @@
 			.centerify{
 			text-align: center;
 			}
-		    .navbar-nav .dropdown-menu .caret {
-				transform: rotate(-90deg);
-		    }
+
 			h1{
 				font-size: 1.7em;
 			}
 			h2{
 				font-size: 1.2em;
 			}
+                      .side-collapse-container{
+              width:100%;
+              position:relative;
+              left:0;
+              transition:left .4s;
+          }
+          .side-collapse-container.out{
+              left:150px;
+          }
+          .side-collapse {
+              top:50px;
+              bottom:0;
+              left:0;
+              width:200px;
+              position:fixed;
+              overflow:hidden;
+              transition:width .4s;
+          }
+          .side-collapse.in {
+              width:0;
+          }
 		}
 
 	@media screen and (min-width:320px) and (max-width:700px) and (orientation:landscape) {
@@ -72,21 +82,60 @@
 			.centerify{
 			text-align: center;
 			}
-		    .navbar-nav .dropdown-menu .caret {
-				transform: rotate(-90deg);
-		    }
+
 			h1{
 				font-size: 2em;
 			}
 			h2{
 				font-size: 1.5em;
 			}
+            .side-collapse-container{
+              width:100%;
+              position:relative;
+              left:0;
+              transition:left .4s;
+          }
+          .side-collapse-container.out{
+              left:150px;
+          }
+          .side-collapse {
+              top:50px;
+              bottom:0;
+              left:0;
+              width:200px;
+              position:fixed;
+              overflow:hidden;
+              transition:width .4s;
+          }
+          .side-collapse.in {
+              width:0;
+          }
+
 	}
 	</style>
 </head>
-<body>
+<body style="background-color: ghostwhite;">
+    <header role="banner" class="navbar navbar-fixed-top navbar-inverse">
+      <div class="container">
+        <div class="navbar-header">
+          <button data-toggle="collapse-side" data-target=".side-collapse" data-target-2=".side-collapse-container" type="button" class="navbar-toggle pull-left"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+        </div>
+        <div class="navbar-inverse side-collapse in">
+          <nav role="navigation" class="navbar-collapse">
+            <ul class="nav navbar-nav">
+              <li><a href="#Wiki">Wiki</a></li>
+              <li><a href="#Team">Team</a></li>
+              <li><a href="#Projects">Projects</a></li>
+              <li><a href="#Events">Events</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+</header>
+<hr>
+<hr>
 <!-- Division 1-->
-	<div class="container-fluid" style="background-color:#eceff1; margin-left: 0.5em;margin-right: 0.5em;height:11em" >
+	<div class="container-fluid" style="background-color:#eceff1; margin-left: 0.5em;margin-right: 0.5em;height:11em;" >
 		<div class="row">
 			<!-- Division 1.1 LOGO-->
 			<div id="div1-1" class="col-xs-1" style="padding: 0px;margin-right: 1em;">
@@ -113,109 +162,10 @@
 			</div>
 		</div>
 		<hr>
-</div>
-        <div class="navbar navbar-default navbar-fixed-top" role="navigation">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">CEDT</a>
-                </div>
-                <div class="collapse navbar-collapse">
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a href="#">Home</a></li>
-                        <li><a href="#" target="_blank">Wiki</a></li>
-                        <li>
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Projects<b class="caret"></b></a>
+    </div>
+        <!-- Nav bar-->
 
-                            <ul class="dropdown-menu">
-                                <li><a href="#">Action [Menu 1.1]</a></li>
-                                <li><a href="#">Another action [Menu 1.1]</a></li>
-                                <li><a href="#">Something else here [Menu 1.1]</a></li>
-                                <li class="divider"></li>
-                                <li><a href="#">Separated link [Menu 1.1]</a></li>
-                                <li class="divider"></li>
-                                <li><a href="#">One more separated link [Menu 1.1]</a></li>
-                                <li>
-                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown [Menu 1.1] <b class="caret"></b></a>
 
-                                    <ul class="dropdown-menu">
-                                        <li><a href="#">Action [Menu 1.2]</a></li>
-                                        <li>
-                                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown [Menu 1.2] <b class="caret"></b></a>
-
-                                            <ul class="dropdown-menu">
-                                                <li>
-                                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown [Menu 1.3] <b class="caret"></b></a>
-
-                                                    <ul class="dropdown-menu">
-                                                        <li><a href="#">Action [Menu 1.4]</a></li>
-                                                        <li><a href="#">Another action [Menu 1.4]</a></li>
-                                                        <li><a href="#">Something else here [Menu 1.4]</a></li>
-                                                        <li class="divider"></li>
-                                                        <li><a href="#">Separated link [Menu 1.4]</a></li>
-                                                        <li class="divider"></li>
-                                                        <li><a href="#">One more separated link [Menu 1.4]</a></li>
-                                                    </ul>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Events <b class="caret"></b></a>
-
-                            <ul class="dropdown-menu">
-                                <li><a href="#">Action [Menu 2.1]</a></li>
-                                <li><a href="#">Another action [Menu 2.1]</a></li>
-                                <li><a href="#">Something else here [Menu 2.1]</a></li>
-                                <li class="divider"></li>
-                                <li><a href="#">Separated link [Menu 2.1]</a></li>
-                                <li class="divider"></li>
-                                <li><a href="#">One more separated link [Menu 2.1]</a></li>
-                                <li>
-                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown [Menu 2.1] <b class="caret"></b></a>
-
-                                    <ul class="dropdown-menu">
-                                        <li><a href="#">Action [Menu 2.2]</a></li>
-                                        <li><a href="#">Another action [Menu 2.2]</a></li>
-                                        <li><a href="#">Something else here [Menu 2.2]</a></li>
-                                        <li class="divider"></li>
-                                        <li><a href="#">Separated link [Menu 2.2]</a></li>
-                                        <li class="divider"></li>
-                                        <li>
-                                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown [Menu 2.2] <b class="caret"></b></a>
-
-                                            <ul class="dropdown-menu">
-                                                <li>
-                                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown [Menu 2.3] <b class="caret"></b></a>
-
-                                                    <ul class="dropdown-menu">
-                                                        <li><a href="#">Action [Menu 2.4]</a></li>
-                                                        <li><a href="#">Another action [Menu 2.4]</a></li>
-                                                        <li><a href="#">Something else here [Menu 2.4]</a></li>
-                                                        <li class="divider"></li>
-                                                        <li><a href="#">Separated link [Menu 2.4]</a></li>
-                                                        <li class="divider"></li>
-                                                        <li><a href="#">One more separated link [Menu 2.4]</a></li>
-                                                    </ul>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div><!--/.nav-collapse -->
-            </div>
-        </div>
 
    
 </body>
@@ -244,19 +194,13 @@
 		}
 		checkWidth();
 		$(window).resize(checkWidth);
-	   $('.navbar a.dropdown-toggle').on('click', function(e) {
-        var $el = $(this);
-        var $parent = $(this).offsetParent(".dropdown-menu");
-        $(this).parent("li").toggleClass('open');
-
-        if(!$parent.parent().hasClass('nav')) {
-            $el.next().css({"top": $el[0].offsetTop, "left": $parent.outerWidth() - 4});
-        }
-
-        $('.nav li.open').not($(this).parents("li")).removeClass("open");
-
-        return false;
-    	});
+        var sideslider = $('[data-toggle=collapse-side]');
+            var sel = sideslider.attr('data-target');
+            var sel2 = sideslider.attr('data-target-2');
+            sideslider.click(function(event){
+                $(sel).toggleClass('in');
+                $(sel2).toggleClass('out');
+            });
 	});
 
 </script>
